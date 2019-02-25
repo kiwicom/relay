74bf211e7fffa87a03ca4e52236fca0d7cbd8420
Martin Zlámal <martin.zlamal@kiwi.com>
2019-02-25 11:31:44 -0600

    Relay: require client identification
    Our clients are forgetting to send `X-Client` header which gives us a hard time when dealing with some failures since we cannot identify who is sending it. This should help - client identification is going to be required when using this package.
    
    Closes: https://gitlab.skypicker.com/incubator/universe/issues/445
    
    kiwicom-source-id: c79ef048b5689c3d4f784e9fa3f064a45e7b415b

--- src/DefaultEnvironment.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-import createEnvironment from './createEnvironment';
-import createNetworkFetcher from './fetchers/createNetworkFetcher';
-
-module.exports = createEnvironment({
-  fetcherFn: createNetworkFetcher('https://graphql.kiwi.com'),
-});
