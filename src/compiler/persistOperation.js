4b1e0472904e7b638818f77cf7d9c401f0642d21
Martin Zlámal <martin.zlamal@kiwi.com>
2019-06-17 16:17:08 -0500

    Relay compiler: prepare persist mutation for CI
    kiwicom-source-id: a4fe387b339ca3f11260866765511da931e1a76c

---
+++ src/compiler/persistOperation.js
@@ -0,0 +1,53 @@
+// @flow
+
+import logger from '@kiwicom/logger';
+
+import graphql from '../graphql';
+import commitMutation from '../commitMutation';
+import createEnvironment from '../createEnvironment';
+import createNetworkFetcher from '../fetchers/createNetworkFetcher';
+
+/**
+ * This mutation persist operations to our GraphQL Persistent Storage for
+ * later direct usage. Update the Relay generated files whith this command:
+ *
+ * yw @kiwicom/relay relay-compiler --src=./src/compiler --schema=../../incubator/graphql.kiwi.com.schema
+ */
+export default function persistOperation(operationId: string, text: string) {
+  return commitMutation(
+    // $FlowExpectedError: `createEnvironment` should not be used directly but we are not in React context here
+    createEnvironment({
+      logger: false,
+      fetchFn: createNetworkFetcher('https://graphql.kiwi.com/', {
+        'X-Client': 'Kiwi.com Relay Compiler',
+      }),
+    }),
+    {
+      mutation: graphql`
+        mutation persistOperationMutation($input: [StoredOperationInput!]!) {
+          createStoredOperations(persistedOperations: $input) {
+            createdOperations {
+              operationId
+              text
+            }
+          }
+        }
+      `,
+      variables: {
+        input: { operationId, text },
+      },
+      onError: error => {
+        logger.error('TODO: onError', String(error));
+        process.exitCode = 1;
+      },
+      onCompleted: (response, errors) => {
+        logger.error(
+          'TODO: onCompleted',
+          String(JSON.stringify(response)),
+          String(errors),
+        );
+        process.exitCode = 2;
+      },
+    },
+  );
+}
