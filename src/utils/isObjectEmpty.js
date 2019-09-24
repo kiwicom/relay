c8f189e334575a028e043fa4a7cc7aa6307ac9c6
Martin Zlámal <mrtnzlml@gmail.com>
2019-09-24 11:09:50 -0500

    @kiwicom/js: add 'isObjectEmpty' function
    kiwicom-source-id: 0528b5402bb65537934e0b938859e4568ffdc4c8

--- src/utils/isObjectEmpty.js
+++
@@ -1,8 +0,0 @@
-// @flow strict
-
-import { isObject } from '@kiwicom/js';
-
-export default function isObjectEmpty(obj: mixed): boolean %checks {
-  // https://stackoverflow.com/a/32108184/3135248
-  return isObject(obj) && Object.entries(obj).length === 0 && obj.constructor === Object;
-}
